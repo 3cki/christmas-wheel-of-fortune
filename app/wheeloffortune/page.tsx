bf0037c692a00a0e8978308890c567375c521d47
Maximilian Eckert <eckertm2000@gmail.com>
2024-12-23 00:08:48 +0100

    full screen controls

--- app/wheeloffortune/page.tsx
+++ app/wheeloffortune/page.tsx
@@ -1,7 +1,13 @@
 "use client";
 import Table from "@/app/components/table";
 import Wheel from "@/app/components/Wheel";
+import { Button } from "@nextui-org/react";
+import {
+  ArrowsPointingOutIcon,
+  ArrowsPointingInIcon,
+} from "@heroicons/react/24/solid";
 import React, { useState } from "react";
+import { FullScreen, useFullScreenHandle } from "react-full-screen";
 
 export type CurrentGame =
   | "normal"
@@ -26,15 +32,41 @@ const WheelOfFortune = () => {
   const [spinning, setSpinning] = useState<boolean>(false);
   const [selectedSlice, setSelectedSlice] = useState<Slice | null>(null);
 
+  const fullScreen = useFullScreenHandle();
+
   return (
-    <div className="h-screen flex items-center justify-evenly">
-      <Wheel
-        spinning={spinning}
-        setSpinning={setSpinning}
-        setSelectedSlice={setSelectedSlice}
-      />
-      <Table />
-    </div>
+    <FullScreen handle={fullScreen}>
+      <div className="h-screen flex flex-col items-center justify-between screen">
+        <div className="w-full flex items-center justify-end gap-4 p-4">
+          <Button
+            isIconOnly
+            startContent={
+              fullScreen.active ? (
+                <ArrowsPointingInIcon className="w-6 h-6" />
+              ) : (
+                <ArrowsPointingOutIcon className="w-6 h-6" />
+              )
+            }
+            onPress={() => {
+              if (fullScreen.active) {
+                fullScreen.exit();
+              } else {
+                fullScreen.enter();
+              }
+            }}
+          />
+        </div>
+        <div className="flex items-center justify-evenly">
+          <Wheel
+            spinning={spinning}
+            setSpinning={setSpinning}
+            setSelectedSlice={setSelectedSlice}
+          />
+          <Table />
+        </div>
+        <div></div>
+      </div>
+    </FullScreen>
   );
 };
 
