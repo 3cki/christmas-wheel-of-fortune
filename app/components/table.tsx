c95f411b13da1005b2e52831a772513926a90cfb
Maximilian Eckert <eckertm2000@gmail.com>
2024-12-22 23:50:30 +0100

    clean and persistent

--- app/components/table.tsx
+++ app/components/table.tsx
@@ -1,31 +1,38 @@
-import { Button, Input } from "@nextui-org/react";
+import { Button, Input, Spacer } from "@nextui-org/react";
 import { TrashIcon, MinusIcon, PlusIcon } from "@heroicons/react/24/solid";
 import { Participant, useParticipantsStore } from "@/app/state/participants";
 import { useState } from "react";
+import { div } from "framer-motion/client";
 
 export default function Table() {
-  const { participants, addParticipant } = useParticipantsStore();
-  const [newParticipantName, setNewParticipantName] = useState("");
+  const { participants } = useParticipantsStore();
 
   return (
-    <div className="w-1/2 p-4 bg-white rounded grid grid-cols-6 gap-4">
-      <div className="font-bold">Name</div>
-      <div className="font-bold">Geschenke</div>
-      <div className="font-bold">Name</div>
-      <div className="font-bold">Geschenke</div>
-      <div className="font-bold">Name</div>
-      <div className="font-bold">Geschenke</div>
+    <div className="w-1/2 p-4 bg-white rounded-lg grid grid-cols-3 gap-4 border border-dotted border-red-400 border-8">
+      <Header />
       {participants.map((participant) => (
         <ParticipantRow participant={participant} key={participant.id} />
       ))}
-      <Input
-        variant="bordered"
-        value={newParticipantName}
-        onChange={(e) => setNewParticipantName(e.target.value)}
-      />
-      <Button onPress={() => addParticipant(newParticipantName)}>
-        Teilnehmer hinzufügen
-      </Button>
+      <AddParticipantRow />
+    </div>
+  );
+}
+
+function Header() {
+  return (
+    <>
+      <HeaderRow />
+      <HeaderRow />
+      <HeaderRow />
+    </>
+  );
+}
+
+function HeaderRow() {
+  return (
+    <div className="flex items-center justify-between">
+      <div className="font-bold">Name</div>
+      <div className="font-bold">Geschenke</div>
     </div>
   );
 }
@@ -35,28 +42,57 @@ function ParticipantRow({ participant }: { participant: Participant }) {
     useParticipantsStore();
 
   return (
-    <>
-      <div>
+    <div className="flex items-center justify-between">
+      <div className="flex items-center gap-2">
         <Button
           isIconOnly
-          startContent={<TrashIcon className="h-6 w-6" />}
+          size="sm"
+          color="danger"
+          variant="flat"
+          startContent={<TrashIcon className="h-4 w-4" />}
           onPress={() => removeParticipant(participant.id)}
         />
         {participant.name}
       </div>
-      <div>
+      <div className="flex items-center gap-2">
         <Button
           isIconOnly
-          startContent={<MinusIcon className="h-6 w-6" />}
+          size="sm"
+          variant="flat"
+          startContent={<MinusIcon className="h-4 w-4" />}
           onPress={() => decreaseGifts(participant.id)}
         />
         {participant.gifts}
         <Button
           isIconOnly
-          startContent={<PlusIcon className="h-6 w-6" />}
+          size="sm"
+          color="primary"
+          startContent={<PlusIcon className="h-4 w-4" />}
           onPress={() => increaseGifts(participant.id)}
         />
       </div>
-    </>
+    </div>
+  );
+}
+
+function AddParticipantRow() {
+  const { addParticipant } = useParticipantsStore();
+  const [newParticipantName, setNewParticipantName] = useState("");
+
+  return (
+    <div className="col-span-3 flex items-center gap-4">
+      <Input
+        variant="bordered"
+        value={newParticipantName}
+        onChange={(e) => setNewParticipantName(e.target.value)}
+      />
+      <Button
+        onPress={() => addParticipant(newParticipantName)}
+        color="primary"
+        variant="flat"
+      >
+        Hinzufügen
+      </Button>
+    </div>
   );
 }
--- app/state/participants.ts
+++ app/state/participants.ts
@@ -1,4 +1,5 @@
 import { create } from "zustand";
+import { persist, createJSONStorage } from "zustand/middleware";
 
 export interface Participant {
   id: string;
@@ -14,41 +15,49 @@ interface ParticipantsState {
   decreaseGifts: (id: string) => void;
 }
 
-export const useParticipantsStore = create<ParticipantsState>((set) => ({
-  participants: [
-    {
-      id: "1",
-      name: "Max",
-      gifts: 0,
-    },
-  ],
-  addParticipant: (name: string) =>
-    set((state) => ({
+export const useParticipantsStore = create<ParticipantsState>()(
+  persist(
+    (set) => ({
       participants: [
-        ...state.participants,
-        { id: Math.random().toString(), name, gifts: 0 },
+        {
+          id: "1",
+          name: "Max",
+          gifts: 0,
+        },
       ],
-    })),
-  removeParticipant: (id: string) =>
-    set((state) => ({
-      participants: state.participants.filter(
-        (participant) => participant.id !== id
-      ),
-    })),
-  increaseGifts: (id: string) =>
-    set((state) => ({
-      participants: state.participants.map((participant) =>
-        participant.id === id
-          ? { ...participant, gifts: participant.gifts + 1 }
-          : participant
-      ),
-    })),
-  decreaseGifts: (id: string) =>
-    set((state) => ({
-      participants: state.participants.map((participant) =>
-        participant.id === id
-          ? { ...participant, gifts: participant.gifts - 1 }
-          : participant
-      ),
-    })),
-}));
+      addParticipant: (name: string) =>
+        set((state) => ({
+          participants: [
+            ...state.participants,
+            { id: Math.random().toString(), name, gifts: 0 },
+          ],
+        })),
+      removeParticipant: (id: string) =>
+        set((state) => ({
+          participants: state.participants.filter(
+            (participant) => participant.id !== id
+          ),
+        })),
+      increaseGifts: (id: string) =>
+        set((state) => ({
+          participants: state.participants.map((participant) =>
+            participant.id === id
+              ? { ...participant, gifts: participant.gifts + 1 }
+              : participant
+          ),
+        })),
+      decreaseGifts: (id: string) =>
+        set((state) => ({
+          participants: state.participants.map((participant) =>
+            participant.id === id
+              ? { ...participant, gifts: participant.gifts - 1 }
+              : participant
+          ),
+        })),
+    }),
+    {
+      name: "participants-storage", // unique name for the storage
+      storage: createJSONStorage(() => localStorage), // using localStorage
+    }
+  )
+);
